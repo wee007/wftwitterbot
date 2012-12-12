f9af1cdc4d28cf425736baeab8fedf8347378fb7
Westfield <westfield@Snow-Leopards-MacBook-Pro.local>
2012-12-13 01:32:06 +1100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wflabs::Application.config.secret_token = 'd7564f674d38faa10d7babb712ce54587fa5150d773640a66ccb7635a00a667e587dd8d6f544d870857d8560139ec028d77be6bd1cc695aa2d0d038d56a472fa'
