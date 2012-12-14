12a4e0919267306aa2a8665c76cfcafcd535d491
Eric Wee <wee007@gmail.com>
2012-12-14 14:48:53 +1100

    [TASK] Create sample products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create(name: "Highstreet Toggle Jacket", brand: "Henleys", url: "http://www.westfield.com.au/au/retailers/henleys/products/highstreet-toggle-jacket~G15471865")
+Product.create(name: "H Y Jacket", brand: "Henleys", url: "http://www.westfield.com.au/au/retailers/henleys/products/h-y-jacket~G15647142")
+Product.create(name: "Xavier Jacket in Black", brand: "Henleys", url: "http://www.westfield.com.au/au/retailers/henleys/products/xavier-jacket-in-black~G15930696")
+Product.create(name: "Santa Ana Jacket in Camo", brand: "Henleys", url: "http://www.westfield.com.au/au/retailers/henleys/products/santa-ana-jacket-in-camo~G15930514")
+
+Product.create(name: "Margot Dress", brand: "Rodeo Show", url: "http://www.westfield.com.au/au/retailers/rodeo-show/products/margot-dress~MARGOT_DRESS")
+Product.create(name: "Halycon Drape Tunic", brand: "Rodeo Show", url: "http://www.westfield.com.au/au/retailers/rodeo-show/products/halycon-drape-tunic~HALYCON%20DRAPE_TUNIC")
+Product.create(name: "Dreamweaver Dress", brand: "Rodeo Show", url: "http://www.westfield.com.au/au/retailers/rodeo-show/products/dreamweaver-dress~DREAMWEAVER_DRESS")
+Product.create(name: "Mimosa Plain Swing Dress", brand: "Rodeo Show", url: "http://www.westfield.com.au/au/retailers/rodeo-show/products/mimosa-plain-swing-dress~MIMOSA_PLAIN_SWING_DRESS")
+
+Product.create(name: "Spotlight Small Flap", brand: "Guess", url: "http://www.westfield.com.au/au/retailers/guess/products/spotlight-small-flap~VG377118")
+Product.create(name: "Siggi Top Zip", brand: "Guess", url: "http://www.westfield.com.au/au/retailers/guess/products/siggi-top-zip~VG376111")
+Product.create(name: "Neta Clutch", brand: "Guess", url: "http://www.westfield.com.au/au/retailers/guess/products/neta-clutch~SG376927http://www.westfield.com.au/au/retailers/guess/products/nixa-frame~OG363715")
+Product.create(name: "Nixa Frame", brand: "Guess", url: "http://www.westfield.com.au/au/retailers/guess/products/nixa-frame~OG363715")
